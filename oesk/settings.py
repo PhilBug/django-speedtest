19eaa75cf990867e546b4274d866051bbb6e785c
fbugaj <fbugaj@match-trade.com>
2021-01-28 01:01:15 +0100

    initial commit

--- oesk/settings.py
+++ oesk/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'charts.apps.ChartsConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'oesk/static'
+]
+
+# Media folder settings
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
